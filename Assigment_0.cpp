19032d751d8a5837e3c70d701982ef4b45e96a74
Pablo <pabl3inz@gmail.com>
2020-08-26 00:28:40 -0500

    Assignment 0 Submission

---
+++ Assigment_0.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+
+	std::cout<<"I have confirmed that I observed the output 'hello world' using the source code\n"
+		<<"-- hello.cpp (in C++)\n"
+		<<"-- hello.m (in Matlab)\n"
+		<<"-- hello.py (in Python)\n";
+	
+	return 0;
+}
+
