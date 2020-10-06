988c6f527be721976db2637909aee1e1e7a8c461
Pablo <pabl3inz@gmail.com>
2020-10-05 23:48:36 -0500

    added v3 of HW4

--- HW 4/HW4_2c.m
+++ HW 4/HW4_2c.m
@@ -16,10 +16,63 @@ max_V=V(:,imax_e);
 
 max_lV=max_e*max_V;
 b_old=b;
+kmax=0;
 while 1
+   
     b_new=M*b_old/norm(b_old);
     if (norm(b_new - b_old) <1.0e-6*norm(b_new))
         break;
     end
+     kmax=kmax+1;
     b_old=b_new;
 end
+i=1:1:50;
+i=i';
+error=abs(max_lV-b_new)./max_lV*100;
+
+f1 = figure;
+f2 = figure;
+f3 = figure;
+
+figure(f1);
+
+plot(i,error,'x')
+title('Error in maximum eigenvector determination');
+legend('error(iter vs numerical)');
+box on
+ax=gca;
+ax.FontSize=12;
+xlabel('entry');
+ylabel('error(%)');
+
+saveas(gcf,'HW4_2error','epsc');
+
+figure(f2);
+hold all;
+plot(i,max_lV,'or')
+plot(i,b_new,'xk')
+plot(i,error,'x')
+title('Maximum eigenvector determination');
+legend('Max eigenvector');
+box on
+ax=gca;
+ax.FontSize=12;
+xlabel('entry');
+ylabel('Max eigenvector component');
+saveas(gcf,'HW4_2cd','epsc');
+
+hold off;
+
+
+figure(f3);
+hold all;
+errorbar(i,b_new,error)
+title('Maximum eigenvector determination');
+legend('Max eigenvector');
+box on
+ax=gca;
+ax.FontSize=12;
+xlabel('entry');
+ylabel('Max eigenvector component');
+saveas(gcf,'HW4_2cdbars','epsc');
+hold off;
