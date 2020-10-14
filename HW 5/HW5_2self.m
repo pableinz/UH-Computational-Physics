35bc565f3b47c3f1842d6048bc56c9797ff5a170
Pablo <pabl3inz@gmail.com>
2020-10-14 08:57:59 -0500

    added vfinal of HW5

--- HW 5/HW5_2self.m
+++ HW 5/HW5_2self.m
@@ -1,16 +1,33 @@
 clear;
+tic
+syms x y;
+r = sym('x_%d',[2 1]);
+F=@(r) r(1)^2*exp(-r(1)^2)+r(2)^2-1;
+G=@(r) r(1)^4/(1+r(1)^2*r(2)^2)-1;
 
-F=@(x,y) x.^2.*exp(-x.^2)+y.^2;
-G=@(x,y) x.^4./(1+x.^2.*y.^2);
-
-x0=[1];
-y0=[1];
-x=[];
-y=[];
+h=@(r) [F(r);G(r)];
+J=@(r) jacobian(h(r),[r]);
+Jinv=@(r) inv(J(r));
+%Jinv0=subs(Jinv([x;y]),{x,y},{r0(1),r0(2)}) %for testing
+r0=[-1;-1];
+rold=r0;
+rnew=[0;0];
 iter=1;
+sx=[];%for storing and analyzing convergence
+sy=[];
+tol=1e-6;
 while(1)
-   
-    x(iter+1)=x(iter)-F(x(iter),y(1))
+    rnew=rold-vpa(subs(Jinv([x;y]),{x,y},{rold(1),rold(2)}))*vpa(subs(h([x;y]),{x,y},{rold(1),rold(2)}));
+    sx(iter)=(rnew(1))
+    sy(iter)=(rnew(2))
+    rold=rnew;
     
     iter=iter+1;
+    if(vpa(subs(h([x;y]),{x,y},{rold(1),rold(2)}))<tol)
+        break;
+    end
 end
+root=vpa(rnew)
+toc
+%1.1739777098108600266775112342048
+%0.80786881713905550481258636604307
--- HW 5/HW5_3alt.m
+++ HW 5/HW5_3alt.m
@@ -1,61 +1,86 @@
 clear;
-fitEqn = fittype('a*exp(-t/b)','independent','t');
-fitEqn2 = fittype('a*exp(-t/b)+c*exp(-t/d)','independent','t');
-startPoints = [0.1 30];
-startPoints2 = [2 10 10 5];
+fitEqn = fittype('a*exp(-t/tau)','independent','t');
+fitEqn2 = fittype('a*exp(-t/tau1)+b*exp(-t/tau2)','independent','t');
+startPoints = [30 1];
+startPoints2 = [30 1 50 10];
 
-options = fitoptions('Method', 'NonLinearLeastSquares','Start', startPoints,'TolFun',1e-8)
-options2 = fitoptions('Method', 'NonLinearLeastSquares','Start', startPoints2,'TolFun',1e-8)
+options = fitoptions('Method', 'NonLinearLeastSquares','Start', startPoints,'TolFun',1e-8);
+options2 = fitoptions('Method', 'NonLinearLeastSquares','Start', startPoints2,'TolFun',1e-8);
 data1=load('data_1.csv');
 data1xvals=data1(:,1);
 data1yvals=data1(:,2);
 
-[d1f1, d1good1, d1out1] = fit(data1xvals,data1yvals,fitEqn,options);
-[d1f2, d1good2, d1out2] = fit(data1xvals,data1yvals,fitEqn2,options2);
+[d1f1, d1f1good, d1f1out] = fit(data1xvals,data1yvals,fitEqn,options);
+[d1f2, d1f2good, d1f2out] = fit(data1xvals,data1yvals,fitEqn2,options2);
 
 f1=figure;
-plot(d1f1,data1xvals,data1yvals)
+pd1=plot(data1xvals,data1yvals,'.b');
 hold on
-plot(d1f2,'k',data1xvals,data1yvals)
-saveas(gcf,'HW5_3_d1fitIC1','epsc');
+pd1f1=plot(d1f1,'-k');
+pd1f2=plot(d1f2,'-r');
+legend([pd1 pd1f1 pd1f2],['data 1'],['fitEqn1 R^2=' num2str(d1f1good.rsquare)],['fitEqn2 R^2=' num2str(d1f2good.rsquare)]);
+title('Fits for data 1');
+box on
+ax=gca;
+xlabel('t');
+ylabel('x(t)');
+%labels the axes 
+saveas(gcf,'HW5_3_d1fitIC1s3','epsc');
 hold off
 
 
 data2=load('data_2.csv');
 data2xvals=data2(:,1);
 data2yvals=data2(:,2);
-startPoints = [1 30];
-startPoints2 = [10 3 1 30];
-options = fitoptions('Method', 'NonLinearLeastSquares','Start', startPoints,'TolFun',1e-8)
-options2 = fitoptions('Method', 'NonLinearLeastSquares','Start', startPoints2,'TolFun',1e-8)
+startPoints = [30 1];
+startPoints2 = [3 10 30 1];
+options = fitoptions('Method', 'NonLinearLeastSquares','Start', startPoints,'TolFun',1e-8);
+options2 = fitoptions('Method', 'NonLinearLeastSquares','Start', startPoints2,'TolFun',1e-8);
 
-[d2f1, d2good1, d2out1] = fit(data2xvals,data2yvals,fitEqn,options);
-[d2f2, d2good2, d2out2] = fit(data2xvals,data2yvals,fitEqn2,options2);
+[d2f1, d2f1good, d2f1out] = fit(data2xvals,data2yvals,fitEqn,options);
+[d2f2, d2f2good, d2f2out] = fit(data2xvals,data2yvals,fitEqn2,options2);
 f2=figure;
-plot(d2f1,data2xvals,data2yvals);
+pd2=plot(data2xvals,data2yvals,'.b');
 hold on
-plot(d2f2,'k',data2xvals,data2yvals);
-saveas(gcf,'HW5_3_d2fitIC1','epsc');
+pd2f1=plot(d2f1,'-k');
+pd2f2=plot(d2f2,'-r');
+legend([pd2 pd2f1 pd2f2],['data 2'],['fitEqn1 R^2=' num2str(d2f1good.rsquare)],['fitEqn2 R^2=' num2str(d2f2good.rsquare)]);
+title('Fits for data 2');
+box on
+ax=gca;
+xlabel('t');
+ylabel('x(t)');
+%labels the axes 
+
+saveas(gcf,'HW5_3_d2fitIC1s3','epsc');
 hold off
 
 
 data3=load('data_3.csv');
 data3xvals=data3(:,1);
 data3yvals=data3(:,2);
-startPoints = [1 1];
-startPoints2 = [10 10 5 300];
-options = fitoptions('Method', 'NonLinearLeastSquares','Start', startPoints,'TolFun',1e-8)
-options2 = fitoptions('Method', 'NonLinearLeastSquares','Start', startPoints2,'TolFun',1e-8)
+startPoints = [1 30];
+startPoints2 = [1 2 30 10];
+options = fitoptions('Method', 'NonLinearLeastSquares','Start', startPoints,'TolFun',1e-8);
+options2 = fitoptions('Method', 'NonLinearLeastSquares','Start', startPoints2,'TolFun',1e-8);
 
-[d3f1, d3good1, d3out1] = fit(data3xvals,data3yvals,fitEqn,options);
-d3rsf1=d3good1.rsquare
+[d3f1, d3f1good, d3f1out] = fit(data3xvals,data3yvals,fitEqn,options);
+%d3rsf1=d3f1good.rsquare
 
-[d3f2, d3good2, d3out2] = fit(data3xvals,data3yvals,fitEqn2,options2);
-d3rsf2=d3good2.rsquare
+[d3f2, d3f2good, d3f2out] = fit(data3xvals,data3yvals,fitEqn2,options2);
+%d3rsf2=d3f2good.rsquare
 
 f3=figure;
-plot(d3f1,data3xvals,data3yvals);
+pd3=plot(data3xvals,data3yvals,'.b');
 hold on
-plot(d3f2,'k',data3xvals,data3yvals);
-saveas(gcf,'HW5_3_d3fitIC1','epsc');
+pd3f1=plot(d3f1,'-k');
+pd3f2=plot(d3f2,'-r');
+legend([pd3 pd3f1 pd3f2],['data 3'],['fitEqn1 R^2=' num2str(d3f1good.rsquare)],['fitEqn2 R^2=' num2str(d3f2good.rsquare)]);
+title('Fits for data 3');
+box on
+ax=gca;
+xlabel('t');
+ylabel('x(t)');
+saveas(gcf,'HW5_3_d3fitIC1s3','epsc');
+
 hold off
